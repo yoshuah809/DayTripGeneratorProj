73233ea086097dbef40fc34a26c27987119e62ea
unknown <yoshuah809@gmail.com>
2021-09-24 12:21:55 -0500

    Init - Loaded the arrays to the JS code

---
+++ app.js
@@ -0,0 +1,30 @@
+// Learning objective: Use and practice JavaScript fundamentals, with an emphasis on Single Responsibility.
+//(5 points): As a developer, I want to make at least three commits.
+
+// (5 points): As a user, I want a destination to be randomly selected for my day trip.
+
+//5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+const restaurantArray = [
+  "Crackle Barrel",
+  "Aplebees",
+  "Chillis",
+  "Seafood Shack",
+  "El Papasito",
+];
+const destinationsArray = [
+  "Los Angeles",
+  "Seattle",
+  "Grand Canyon",
+  "San Francisco",
+  "New York",
+  "Cancun",
+];
+const arrayOfTransportation = ["Air Plane", "By Bus", "By Cruise", "Car"];
+const arrayOfEntertainment = [
+  "Mountain Climbing",
+  "Music Concert",
+  "Theather",
+  "Sports",
+  "Scuba Diving",
+  "Dirty Racing",
+];
