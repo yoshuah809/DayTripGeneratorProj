bdc4542cf7838c1b1c7ab0195df92616e07280db
unknown <yoshuah809@gmail.com>
2021-09-27 08:32:29 -0500

    Added the ChangeTrip option if desired to change any option

--- app.js
+++ app.js
@@ -51,3 +51,14 @@ alert(`              Destination is: ${destination}
         Entertainment is: : ${entertainment}
         Would you like to change anything, Select OK to continue.`);
 changeTrip();
+
+function changeTrip() {
+  changeOption = Number(
+    prompt(`Would to like to change anything, Select an option
+         1. Destination    --  Current is ${destination}
+         2. Transportation --  Current is ${transportation}
+         3. Restaurant     --  Current is ${restaurant}
+         4. Entertainment  --  Current is ${entertainment}
+         5. No change Needed Confirm Trip.`)
+  );
+}
